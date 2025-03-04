e4b8b9db542cc6d6eebfcc1eeef1e63b627567a8
daniyal-p <daniyal733khan@gmail.com>
2025-03-04 15:54:09 +0530

    =elrkn

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,7 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import netlify from "@astrojs/netlify";
+import netlify from "netlify";
 
 // https://astro.build/config
 export default defineConfig({
