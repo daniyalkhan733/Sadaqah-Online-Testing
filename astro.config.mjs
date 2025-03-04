7ff4dfd357e8e48f7f69b7a4904d0f212fec7f69
daniyal-p <daniyal733khan@gmail.com>
2025-03-04 15:49:05 +0530

    update

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,12 +1,12 @@
 import { defineConfig } from "astro/config";
 import tailwind from "@astrojs/tailwind";
 import react from "@astrojs/react";
-import cloudflare from "@astrojs/cloudflare";
+import netlify from "@astrojs/netlify";
 
 // https://astro.build/config
 export default defineConfig({
   integrations: [tailwind(), react()],
   output: "server",
-  adapter: cloudflare(),
+  adapter: netlify(),
   site: "https://mysadaqahonline.com/",
 });
